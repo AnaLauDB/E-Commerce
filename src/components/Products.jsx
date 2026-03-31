e7e845148f257e100a7acb4128e771a3f1ba4217
AnaLauDB <analau3217@gmail.com>
2026-03-31 13:29:49 -0600

    Componente de logica productos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,24 +1,46 @@
-import { AddToCartIcon } from './Icons'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons'
 import './Products.css'
+import { useCart } from '../hooks/useCart'
 
 export function Products({ products }) {
+
+    const { addToCart, removeFromCart, cart } = useCart()
+
+    const checkIfProductInCart = (product) => {
+        return cart.some(item => item.id === product.id)
+    }
 
     return (
         <main className='products'>
             <ul>
-                {products.map(product => (
-                    <li key={product.id}>
-                        <img src={product.thumbnail} alt={product.title} />
-                        <div>
-                            <strong>{product.title}</strong> - ${product.price}
-                        </div>
-                        <div>
-                            <button>
-                                <AddToCartIcon />
-                            </button>
-                        </div>
-                    </li>
-                ))}
+                {products.slice(0, 10).map(product => {
+                    const isProductInCart = checkIfProductInCart(product)
+
+                    return (
+                        <li key={product.id}>
+                            <img
+                                src={product.thumbnail}
+                                alt={product.title}
+                            />
+                            <div>
+                                <strong>{product.title}</strong> - ${product.price}
+                            </div>
+                            <div>
+                                <button nClick={() => {
+                                    isProductInCart
+                                        ? removeFromCart(product)
+                                        : addToCart(product)
+                                }}>
+                                    {
+                                        isProductInCart
+                                            ? <RemoveFromCartIcon />
+                                            : <AddToCartIcon />
+                                    }
+                                </button>
+                            </div>
+                        </li>
+                    )
+                })}
             </ul>
         </main>
     )
