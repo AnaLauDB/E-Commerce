4d0af8510354663ecbb3bcc467151d3de8efaeb4
AnaLauDB <analau3217@gmail.com>
2026-03-31 11:50:07 -0600

    add: componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+
+export function Footer() {
+    return (
+        <footer className='footer'>
+            <p>© 2024 E-Commerce. All rights reserved.</p>
+            <p> Shopping Cart con useContext y useReducer</p>
+        </footer>
+    )
+}
