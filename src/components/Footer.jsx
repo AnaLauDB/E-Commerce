15ce6ae9a0f2e0e57a2a1a726efa229f322ccb9d
AnaLauDB <analau3217@gmail.com>
2026-03-31 13:29:13 -0600

    add: componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import './Footer.css'
+import { useCart } from '../hooks/useCart'
 
 export function Footer() {
+    const { cartItems } = useCart()
+
     return (
         <footer className='footer'>
             <p>© 2024 E-Commerce. All rights reserved.</p>
             <p> Shopping Cart con useContext y useReducer</p>
+            {
+                JSON.stringify(cartItems, null, 2)
+            }
         </footer>
     )
 }
